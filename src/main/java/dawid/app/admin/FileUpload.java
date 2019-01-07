7c9dd36a2d36aa725da3ebfecedc651560521d39
milkerso <dkrawczyk53@wp.pl>
2019-01-07 18:47:49 +0100

    Register and log

---
+++ src/main/java/dawid/app/admin/FileUpload.java
@@ -0,0 +1,16 @@
+package dawid.app.admin;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUpload {
+	
+	private MultipartFile filename;
+
+	public MultipartFile getFilename() {
+		return filename;
+	}
+
+	public void setFilename(MultipartFile filename) {
+		this.filename = filename;
+	}
+}
